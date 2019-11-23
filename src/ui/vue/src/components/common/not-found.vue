c10680e1b74d57bd54e2b1dd14d3648de0372fb3
Kare Koho <kare.koho@yahoo.com>
2019-11-23 13:21:14 +0200

    Add not found route

---
+++ src/ui/vue/src/components/common/not-found.vue
@@ -0,0 +1,12 @@
+<script>
+export default { name: 'not-found' }
+</script>
+
+<template>
+  <div id="not-found">
+    <h3>Page not found</h3>
+    <router-link :to="{ name: 'index' }">Back to index</router-link>
+  </div>
+</template>
+
+<style scoped />
--- src/ui/vue/src/main.js
+++ src/ui/vue/src/main.js
@@ -4,12 +4,17 @@ import App from './App.vue'
 import store from './services/dpkg'
 import PackageView from './components/package/view'
 import PackageInfo from './components/package/info'
+import NotFound from './components/common/not-found'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
+    {
+      path: '*',
+      component: NotFound
+    },
     {
       path: '/',
       name: 'index',

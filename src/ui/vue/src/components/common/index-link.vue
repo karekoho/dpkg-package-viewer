9caab6c942c8869bd2be1ee1b8604412784f821c
Kare Koho <kare.koho@yahoo.com>
2019-11-23 14:02:44 +0200

    Add index-link component

---
+++ src/ui/vue/src/components/common/index-link.vue
@@ -0,0 +1,9 @@
+<script>
+export default { name: 'index-link' }
+</script>
+
+<template>
+  <router-link id="index-link" :to="{ name: 'index' }">Back to index</router-link>
+</template>
+
+<style scoped />
